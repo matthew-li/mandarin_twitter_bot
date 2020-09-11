eb3b59d5c9763ea1434cd4c7621e885bc15e1f54
Matthew E. Li <matthew.elbert.li@gmail.com>
2020-09-11 15:31:13 -0700

    Modify DynamoDB constants for more convenient access; add/remove Twitter settings

--- constants.py
+++ constants.py
@@ -1,12 +1,52 @@
+from decimal import Decimal
+from enum import Enum
+
+
 class AWSResource(object):
     """"""
     DYNAMO_DB = "dynamodb"
 
 
-class DynamoDBTable(object):
+class DynamoDBSettings(object):
     """"""
-    TWEETS = "Tweets"
-    UNPROCESSED_WORDS = "UnprocessedWords"
+    EARLIEST_TWEET_DATE = "earliest_tweet_date"
+
+
+class DynamoDBTableSchema(object):
+    """"""
+
+    def __init__(self, name, schema):
+        self.name = name
+        self.schema = schema
+
+
+class DynamoDBTable(Enum):
+    """"""
+    SETTINGS = DynamoDBTableSchema(
+        name="Settings",
+        schema={
+            "Name": str,
+            "Value": str,
+        })
+
+    TWEETS = DynamoDBTableSchema(
+        name="Tweets",
+        schema={
+            "Id": str,
+            "TweetId": str,
+            "Date": str,
+            "DateEntry": Decimal,
+            "Word": str,
+        })
+
+    UNPROCESSED_WORDS = DynamoDBTableSchema(
+        name="UnprocessedWords",
+        schema={
+            "Id": str,
+            "Characters": str,
+            "Pinyin": str,
+            "InsertionTimestamp": Decimal,
+        })
 
 
 TWEET_MAX_CHARS = 280
--- settings.py
+++ settings.py
@@ -16,7 +16,8 @@ section = "twitter"
 TWITTER_ACCESS_TOKEN = config.get(section, "twitter_access_token")
 TWITTER_ACCESS_TOKEN_SECRET = config.get(
     section, "twitter_access_token_secret")
+TWITTER_BEARER_TOKEN = config.get(section, "twitter_bearer_token")
 TWITTER_CONSUMER_KEY = config.get(section, "twitter_consumer_key")
 TWITTER_CONSUMER_SECRET = config.get(section, "twitter_consumer_secret")
-TWITTER_USER_ID = config.get(section, "twitter_user_id")
+TWITTER_SIGNATURE_METHOD = "HMAC-SHA1"
 TWITTER_USER_USERNAME = config.get(section, "twitter_user_username")
